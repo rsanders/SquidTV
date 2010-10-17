3a20c3e54dd5452ef1470bdda2f4b58b34994c41
Robert Sanders <robert@esquimaux.org>
2010-10-17 16:34:07 -0400

    files to use mobile_fu (imported next)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,43 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # before_filter :adjust_format_for_iphone
+  has_mobile_fu
 
-  def adjust_format_for_iphone
-    if request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPod)/]
-      request.format = :iphone
+  def is_device_with_cookie?(device)
+    puts "with cookie called!"
+    if cookies[:site_variant] == "normal"
+      false
+    elsif cookies[:site_variant] == device
+      true
+    else
+      is_device_without_cookie?(device)
     end
   end
+
+  def is_mobile_device_with_cookie?
+    puts "imb with cookie called!"
+    if cookies[:site_variant] == "normal"
+      false
+    elsif ! cookies[:site_variant].empty?
+      true
+    else
+      is_mobile_device_without_cookie?
+    end
+  end
+
+  alias_method_chain :is_device?, :cookie
+  alias_method_chain :is_mobile_device?, :cookie
+
+
+#  before_filter :adjust_format_for_mobile
+#
+#  def adjust_format_for_mobile
+#    if cookies[:site_variant] == "mobile" ||
+#            (request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPod|iPad)/] &&
+#                    cookies[:site_variant] != "normal")
+#      request.format = "mobile"
+#      logger.debug "got mobile"
+#    end
+#    true
+#  end
 end
